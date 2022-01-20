9e53e75900b5dfa480520aba95c1af5610d3bcb9
Aleksandr-Molchanov <molchanov_a.v@mail.ru>
2022-01-20 12:57:13 +0300

    0. Управление нитью через wait. [#6858 #253311]

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -13,8 +13,8 @@ public class CountBarrier {
     }
 
     public synchronized void count() {
-        monitor.notifyAll();
         count++;
+        monitor.notifyAll();
     }
 
     public synchronized void await() {
